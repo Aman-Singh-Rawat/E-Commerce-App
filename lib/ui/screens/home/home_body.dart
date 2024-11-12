80823fa3b419db9699052f0cdc768437134ff739
Aman Singh <amanrawat6767@gmail.com>
2024-11-12 12:18:51 +0530

    give finishing to offers CarouselSlider

--- lib/ui/screens/home/home_body.dart
+++ lib/ui/screens/home/home_body.dart
@@ -28,152 +28,157 @@ class _HomeBodyState extends State<HomeBody> {
             : Colors.grey.withAlpha(90),
       ),
       margin: const EdgeInsets.only(right: 6),
-      height: 7,
+      height: 5,
       curve: Curves.easeIn,
-      width: _currentPage == index ? 30 : 7,
+      width: _currentPage == index ? 25 : 5,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: textFieldDecoration.copyWith(
-                hintText: "Search",
-                prefixIcon: const Icon(
-                  Icons.search,
-                  color: Colors.grey,
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Image.asset("assets/images/img_filter.png"),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Column(
               children: [
-                Text(
-                  "Special Offers",
-                  style: GoogleFonts.poppins(
-                    color: const Color(0xFF101010),
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "See All",
-                    style: GoogleFonts.poppins(
-                      color: const Color(0xFF101010),
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17,
+                TextFormField(
+                  decoration: textFieldDecoration.copyWith(
+                    hintText: "Search",
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset("assets/images/img_filter.png"),
                     ),
                   ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            CarouselSlider(
-              items: offersList.map(
-                (offer) {
-                  return Stack(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFE7E7E7),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      offer[Offers.offerDiscount] ?? '',
-                                      style: GoogleFonts.roboto(
-                                        color: const Color(0xFF101010),
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 8),
-                                    Text(
-                                      offer[Offers.offerTitle] ?? '',
-                                      style: GoogleFonts.poppins(
-                                        color: const Color(0xFF101010),
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      offer[Offers.offerSubtitle] ?? '',
-                                      style: GoogleFonts.poppins(
-                                        color: const Color(0xFF101010),
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
-                                child: Image.asset(
-                                  width: double.infinity,
-                                  height: double.infinity,
-                                  offer[Offers.offerImage]!,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            )
-                          ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Special Offers",
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xFF101010),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "See All",
+                        style: GoogleFonts.poppins(
+                          color: const Color(0xFF101010),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17,
                         ),
                       ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(
-                              4,
-                              (listIndex) => _buildDots(index: listIndex),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+          CarouselSlider(
+            items: offersList.map(
+              (offer) {
+                return Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 14),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFE7E7E7),
+                        borderRadius: BorderRadius.circular(28),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    offer[Offers.offerDiscount] ?? '',
+                                    style: GoogleFonts.roboto(
+                                      color: const Color(0xFF101010),
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    offer[Offers.offerTitle] ?? '',
+                                    style: GoogleFonts.poppins(
+                                      color: const Color(0xFF101010),
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    offer[Offers.offerSubtitle] ?? '',
+                                    style: GoogleFonts.poppins(
+                                      color: const Color(0xFF101010),
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
+                          Expanded(
+                            flex: 2,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                width: double.infinity,
+                                height: double.infinity,
+                                offer[Offers.offerImage]!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                            4,
+                            (listIndex) => _buildDots(index: listIndex),
+                          ),
                         ),
-                      )
-                    ],
-                  );
-                },
-              ).toList(),
-              options: CarouselOptions(
-                height: 200,
-                autoPlay: true,
-                onPageChanged: (index, reason) => setState(() {
-                  _currentPage = index;
-                }),
-                viewportFraction: 1.0,
-              ),
-            )
-          ],
-        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ).toList(),
+            options: CarouselOptions(
+              height: 200,
+              autoPlay: true,
+              onPageChanged: (index, reason) => setState(() {
+                _currentPage = index;
+              }),
+              viewportFraction: 1.0,
+            ),
+          )
+        ],
       ),
     );
   }
