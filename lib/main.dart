d67c03ced75505237849e05e04b826e72d458487
Aman Singh <amanrawat6767@gmail.com>
2024-11-18 18:44:03 +0530

    try to fix design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoesy/ui/screens/home/sort_filter_bottom_sheet.dart';
 import 'package:shoesy/ui/screens/shared/bottom_navbar.dart';
 
 void main() => runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
       routes: {
-        "/": (context) => const BottomNavBar(),
+        "/": (context) => const SortFilterBottomSheet(),
         "/bottomNavBar": (context) => const BottomNavBar(),
       },
     );
--- lib/ui/screens/home/sort_filter_bottom_sheet.dart
+++ lib/ui/screens/home/sort_filter_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shoesy/ui/widgets/product_category.dart';
 import 'package:shoesy/ui/widgets/range_slider_widget.dart';
@@ -14,78 +15,89 @@ class SortFilterBottomSheet extends StatefulWidget {
 class _SortFilterBottomSheetState extends State<SortFilterBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 14),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    "Sort & Filter",
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      "Sort & Filter",
+                      style: GoogleFonts.poppins(
+                          color: const Color(0xFF101010),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    width: double.infinity,
+                    height: 1,
+                    color: const Color(0xFFBEBEBE),
+                  ),
+                  Text(
+                    "Categories",
                     style: GoogleFonts.poppins(
                         color: const Color(0xFF101010),
-                        fontSize: 24,
+                        fontSize: 16,
                         fontWeight: FontWeight.w500),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 20),
-                  width: double.infinity,
-                  height: 1,
-                  color: const Color(0xFFBEBEBE),
-                ),
-                Text(
-                  "Categories",
-                  style: GoogleFonts.poppins(
-                      color: const Color(0xFF101010),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: 40,
-            margin: const EdgeInsets.symmetric(vertical: 20),
-            child: const ProductCategory(),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 5),
-            child: Text(
-              "Price Range",
-              style: GoogleFonts.poppins(
-                  color: const Color(0xFF101010),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-          const RangeSliderWidget(),
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Text(
-              "Rating",
-              style: GoogleFonts.poppins(
-                color: const Color(0xFF101010),
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
+                ],
               ),
             ),
-          ),
-          Container(
-            height: 40,
-            margin: const EdgeInsets.symmetric(vertical: 20),
-            child: const ProductCategory(),
-          ),
-          const SizedBox(
-            height: 40,
-            width: double.infinity,
-            child: RatingBarWidget(),
-          ),
-        ],
+            Container(
+              height: 40,
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              child: const ProductCategory(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 5),
+              child: Text(
+                "Price Range",
+                style: GoogleFonts.poppins(
+                    color: const Color(0xFF101010),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+            const RangeSliderWidget(),
+            Container(
+              margin: const EdgeInsets.only(left: 14, right: 14, top: 14, bottom: 9),
+              child: Text(
+                "Sort by",
+                style: GoogleFonts.poppins(
+                  color: const Color(0xFF101010),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+              child: ProductCategory(),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 14, right: 14, top: 14, bottom: 9),
+              child: Text(
+                "Rating",
+                style: GoogleFonts.poppins(
+                  color: const Color(0xFF101010),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+              child: RatingBarWidget(),
+            ),
+          ],
+        ),
       ),
     );
   }
